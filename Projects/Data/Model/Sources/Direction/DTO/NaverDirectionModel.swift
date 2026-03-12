06aa33705b7d44e56bbdd674d6752b5d3f0917da
Roy <shuwj81@daum.net>
2026-03-12 16:45:38 +0900

    fix: 네이버 API 좌표 배열 형식 지원으로 디코딩 에러 해결
    - RoutePoint 모델에 커스텀 디코더 구현
    - 네이버 API의 [경도, 위도] 배열 형식 지원
    - 대안으로 {lat, lng} 객체 형식도 지원
    - DecodingError.typeMismatch 해결

--- Projects/Data/Model/Sources/Direction/DTO/NaverDirectionModel.swift
+++ Projects/Data/Model/Sources/Direction/DTO/NaverDirectionModel.swift
@@ -57,5 +57,33 @@ public struct RouteSummary: Codable {
 public struct RoutePoint: Codable {
     public let lat: Double
     public let lng: Double
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        // 네이버 API는 [lng, lat] 순서의 배열로 좌표를 반환
+        if let coordinateArray = try? container.decode([Double].self) {
+            guard coordinateArray.count >= 2 else {
+                throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "좌표 배열은 최소 2개의 값이 필요합니다."))
+            }
+            self.lng = coordinateArray[0]  // 경도
+            self.lat = coordinateArray[1]  // 위도
+        } else {
+            // 대안: 객체 형태 {lat: ..., lng: ...}
+            let coordinateContainer = try decoder.container(keyedBy: CodingKeys.self)
+            self.lat = try coordinateContainer.decode(Double.self, forKey: .lat)
+            self.lng = try coordinateContainer.decode(Double.self, forKey: .lng)
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(lat, forKey: .lat)
+        try container.encode(lng, forKey: .lng)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case lat, lng
+    }
 }
 
