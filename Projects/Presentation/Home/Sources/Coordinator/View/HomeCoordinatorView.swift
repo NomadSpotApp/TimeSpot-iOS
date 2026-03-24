b8b28fed20d22dde0204579bb3d12151f56ab46b
Roy <shuwj81@daum.net>
2026-03-25 02:53:50 +0900

    feat: Home 모듈 TCA 구조 구현 및 UI 에셋 추가

---
+++ Projects/Presentation/Home/Sources/Coordinator/View/HomeCoordinatorView.swift
@@ -0,0 +1,12 @@
+//
+//  MainCoordinatorView.swift
+//  Home
+//
+//  Created by Wonji Suh  on 3/24/26.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import TCACoordinators
+
+public struct homecoo
