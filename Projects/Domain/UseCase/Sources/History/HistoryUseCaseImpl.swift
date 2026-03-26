e4d15ff1c6b8f7330a9a875236df812c22e90753
Roy <shuwj81@daum.net>
2026-03-26 22:21:22 +0900

    feat: add HistoryUseCaseImpl 및 Profile 수정 #12

---
+++ Projects/Domain/UseCase/Sources/History/HistoryUseCaseImpl.swift
@@ -0,0 +1,41 @@
+//
+//  HistoryUseCaseImpl.swift
+//  UseCase
+//
+//  Created by Wonji Suh  on 3/26/26.
+//
+
+import DomainInterface
+import Entity
+
+import WeaveDI
+import ComposableArchitecture
+
+public struct HistoryUseCaseImpl: HistoryInterface {
+  @Dependency(\.historyRepository) var repository
+
+  public init() {}
+
+  // MARK: - 히스토리 정보
+  public func myHistory(
+    page: Int,
+    size: Int,
+    sort: TravelHistorySort
+  ) async throws -> HistoryEntity {
+    return try await repository.myHistory(page: page, size: size, sort: sort)
+  }
+}
+
+
+extension HistoryUseCaseImpl: DependencyKey {
+  static public var liveValue: HistoryInterface = HistoryUseCaseImpl()
+  static public var testValue: HistoryInterface = HistoryUseCaseImpl()
+  static public var previewValue: HistoryInterface = HistoryUseCaseImpl()
+}
+
+public extension DependencyValues {
+  var historyUseCase: HistoryInterface {
+    get { self[HistoryUseCaseImpl.self] }
+    set { self[HistoryUseCaseImpl.self] = newValue }
+  }
+}
