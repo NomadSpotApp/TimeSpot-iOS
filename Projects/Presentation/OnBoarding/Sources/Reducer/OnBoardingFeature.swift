3b3362459fb9bff861733f2921702d57d1a8b48e
Roy <shuwj81@daum.net>
2026-03-31 02:19:32 +0900

    remove: 비회원 모드 디버그 로그 제거 #16

--- Projects/Presentation/OnBoarding/Sources/Reducer/OnBoardingFeature.swift
+++ Projects/Presentation/OnBoarding/Sources/Reducer/OnBoardingFeature.swift
@@ -158,7 +158,6 @@ extension OnBoardingFeature {
       case .signup:
         // 비회원인 경우 API 통신 없이 바로 onBoardingCompleted로 이동
         if state.userSession.isGuest {
-          #logDebug("비회원 모드 - API 통신 건너뛰고 onBoardingCompleted로 이동")
           return .send(.navigation(.onBoardingCompleted))
         }
 
